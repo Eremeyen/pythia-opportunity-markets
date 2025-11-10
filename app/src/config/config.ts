9e504338028f4cbd4466b3770a984b0144ce1b32
Eremeyen <erenonder1905@gmail.com>
2025-11-10 00:50:27 -0600

    instructions done

--- app/src/config/config.ts
+++ app/src/config/config.ts
@@ -1,1 +1,3 @@
 export const NETWORK: 'mainnet' | 'devnet' = 'devnet';
+export const PYTHIA_PROGRAM_ID = '2qjYCKDKngH2rcnPwpxKgZTdbjvvxq71u7FVChU1541U' as const;
+export type PythiaProgramAddressType = '2qjYCKDKngH2rcnPwpxKgZTdbjvvxq71u7FVChU1541U';
